03f44d52dfcebf528a7ab68eea8ca5b13b6c78a5
nguptani30@gmail.com <nguptani30@gmail.com>
2021-01-21 09:49:42 +0530

    setting db

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql')
+
+var con = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password:"123456",
+    database: "database_packpr"
+
+})
 
 
 app.get('/', (req, res) => {
+    const sqlInsert= "INSER INTO userData(email, password) VALUES ('nitish', 'ngupta@gmail.com');"
+
+    con.query(sqlInsert, (err, result)=> {
+        console.log(err)
+        res.send("hello success")
+    })
     
-   console.log(res.send("hello world")) 
+//    console.log(res.send("hello world")) 
 })
 
 app.listen(3030, () => {
